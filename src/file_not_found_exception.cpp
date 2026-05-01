83b8c2a0381cf3d43697dbe2a805b9d13ecd4089
Bongo Poyo <pineapple123132@gmail.com>
2026-05-01 10:04:18 +0500

    IDK what to say man

--- src/file_not_found_exception.cpp
+++ src/file_not_found_exception.cpp
@@ -1,4 +1,4 @@
-#include"file_not_found_exception"
+#include"file_not_found_exception.hpp"
 // I can create a static or an extern here but ugh
 FileNotFoundException::FileNotFoundException() : HospitalException("Required File cannot be opened")
 {
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
-#include "../sfml/app.hpp"
+// #include "../sfml/app.hpp"
 
-int main()
-{
-    App app;
-    app.run();
-    return 0;
-}
+// int main()
+// {
+//     App app;
+//     app.run();
+//     return 0;
+// }
+
+// #include <SFML/Graphics.hpp>
+// #include "gui_manager.hpp"
+// int main() {
+//     GuiManager gui;
+//     gui.run();
+//     return 0;
+// }
